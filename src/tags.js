e622204d89c80f39ad23a2cef34f16485a3db57d
zangtengfei <81709456@qq.com>
2021-03-23 16:08:24 +0800

    新增 'npm scripts 技巧'

--- src/tags.js
+++ src/tags.js
@@ -7,3 +7,13 @@ export const openwrt = {
   color: 'red',
   text: 'openWrt'
 }
+
+export const engineering = {
+  color: 'yellow',
+  text: '前端工程化'
+}
+
+export const shell = {
+  color: 'blue',
+  text: 'shell'
+}
